3bcdef6d09ebb718503627dcc278c731dddfd6c8
Filip Delden <Filip.delden@gmail.com>
2023-06-14 08:17:00 +0200

    Update 1

--- MainGui.py
+++ MainGui.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from scipy.optimize import minimize
 import copy
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -28,7 +27,7 @@ class SimulationGui():
                      'R': ('Gas constant (kJ/(kg*K))', 0.287),
                      'T': ('Temperature (K)', 293),
                      'pres': ('Inlet pressure (Pa)', 400000),
-                     'dynVisc': ('dynamic viscosity (Pa*s)', 18.1e-6),
+                     'dynVisc': ('Dynamic viscosity (Pa*s)', 18.1e-6),
                      'rho_p': ('Particle Density (kg/m3)', 2000),
                      'dp': ('Particle Diameter (um)', 1e-5),
                      'Mfs': ('Mass flow solids (kg/s)', 0.3, 5.3, 11),
@@ -36,40 +35,47 @@ class SimulationGui():
     entries = {}
     tempSysParam = {}
     pressureDrop = []
+    currentVars = {1:None, 2:None}
 
     def __init__(self):
         # Create gui window called root
         self.root = tk.Tk()
         self.root.wm_title("Pneumatic Conveying GUI")
 
-        # self.root.geometry('800x600')
+        width= self.root.winfo_screenwidth()               
+        height= self.root.winfo_screenheight()               
+        self.root.geometry("%dx%d" % (width, height))
         self.lbl0 = tk.Label(self.root, text="Pneumatic Conveying Simulation - Transportation Pipeline",
-                             font=("Arial", 15))
-        self.lbl0.grid(row=0, columnspan=2, sticky=tk.W+tk.E)
+                             font=("Arial", 15), height=3)
+        self.lbl0.grid(row=0, columnspan=3, sticky=tk.W+tk.E)
 
         self.createHeader()
         self.createMenu()
         self.createLeftFrame()
+        self.createVariableMenuFrame()
 
         # Create a matplotlib canvas
         self.fig = Figure(figsize=(10, 7), dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
-        self.canvas.get_tk_widget().grid(row=2, column=1,padx=30, sticky=tk.W)
+        self.canvas.get_tk_widget().grid(row=2, column=1,columnspan=2,padx=10, sticky=tk.W)
 
         # Initialization run. Start an iterative loop and event loop
         self.afterId = self.root.after(150, self.gui_update)
         self.root.mainloop()
     
-    def update(self):
+    def update_fig(self):
         if len(self.pressureDrop) != 0:
             self.plotData(self.v.get())
         else:          
-            showinfo(title='Incorrect', message='Start by computing, then press update to change the figure')
+            showinfo(title='Incorrect', message='Press calculate, then press update to change the figure')
             return
         
     def save_fig(self):
-        # shows dialog box and return the path to save fig
+        """
+        Saves a figure in the user specified folder path
+        """
+
         path = asksaveasfilename(title='Select Folder',filetypes=[("PNG file", ".png")], defaultextension=".png", confirmoverwrite=True) 
         self.fig.savefig(fname=path,dpi=200)
         return
@@ -80,22 +86,21 @@ class SimulationGui():
 
     def createHeader(self):
         self.headerFrame = tk.Frame(self.root)
-        self.headerFrame.grid(row=1, column=1, padx=20, pady=20)
+        self.headerFrame.grid(row=1, column=2, padx=20, pady=20)
 
         self.v = tk.IntVar(self.headerFrame, 1)
-        self.rb1 = tk.Radiobutton(self.headerFrame, text='Pressure drop',
-                                  variable=self.v, value=1).grid(row=0, column=1, padx=10)
-        self.rb2 = tk.Radiobutton(self.headerFrame, text='Power consumption',
-                                  variable=self.v, value=2).grid(row=0, column=2, padx=10)
-        self.rb3 = tk.Radiobutton(self.headerFrame, text='SLR', variable=self.v, value=3).grid(
+        self.rb1 = ttk.Radiobutton(self.headerFrame, text='Pressure drop',
+                                  variable=self.v, value=1).grid(row=0, column=0, padx=10)
+        self.rb2 = ttk.Radiobutton(self.headerFrame, text='Power consumption',
+                                  variable=self.v, value=2).grid(row=0, column=1, padx=10)
+        self.rb3 = ttk.Radiobutton(self.headerFrame, text='SLR', variable=self.v, value=3).grid(
             row=0, column=3, padx=10)
 
         self.updateButton = ttk.Button(
-            self.headerFrame, text='Update', command=self.update).grid(row=0, column=4, padx=10)
+            self.headerFrame, text='Update', command=self.update_fig).grid(row=0, column=2, padx=10)
         
         self.updateButton = ttk.Button(
-            self.headerFrame, text='Save Figure', command=self.save_fig).grid(row=0, column=5, padx=20,sticky=tk.E)
-
+            self.headerFrame, text='Save Figure', command=self.save_fig).grid(row=0, column=3, padx=20,sticky=tk.E)
 
     def createMenu(self):
         # Create Menu bar
@@ -123,20 +128,40 @@ class SimulationGui():
         help = Menu(menubar, tearoff=0)  
         help.add_command(label="About", command=None)  
         menubar.add_cascade(label="Help", menu=help)  
-            
+
         self.root.config(menu=menubar)
 
+    def createVariableMenuFrame(self):
+        self.variableFrame = tk.Frame(self.root)
+        self.variableFrame.grid(row=1, column=0, columnspan=2,padx=10, pady=20, sticky=tk.W)
+        
+        #Set the Menu initially
+        self.menu1= tk.StringVar()
+        self.menu1.set('Variable 1')
+
+        self.menu2= tk.StringVar()
+        self.menu2.set('Variable 2')
+
+        variableList=[]
+        for key, value in self.sysParameters.items():
+            variableList.append(value[0])
+        
+        #Create a dropdown Menu
+        drop1 = tk.OptionMenu(self.variableFrame, self.menu1,*variableList,command = lambda x: self.updateLeftFrame(self.menu1.get(), 1)).grid(row=0, column=0, padx=10, sticky='W')
+        drop2 = tk.OptionMenu(self.variableFrame, self.menu2,*variableList,command = lambda x: self.updateLeftFrame(self.menu2.get(), 2)).grid(row=0, column=1, padx=10, sticky='W')
+    
 
     def createLeftFrame(self):
         # Create the left frame
         self.leftFrame = tk.Frame(self.root)
-        self.leftFrame.grid(row=2, column=0, padx=20, pady=20)
+        self.leftFrame.grid(row=2, column=0, padx=10, pady=20)
 
         counter = 2
 
         for param in self.sysParameters:
             entriesList = []
-            if len(self.sysParameters[param]) == 4:
+            entriesList.append(counter)
+            """if len(self.sysParameters[param]) == 4:
                 self.massFlows = tk.Frame(self.leftFrame)
                 self.massFlows.grid(row=counter, column=1)
                 if param == 'Mfs':
@@ -159,14 +184,14 @@ class SimulationGui():
                     entry.insert(0, self.sysParameters[param][i])
                     entriesList.append(entry)
 
-            elif len(self.sysParameters[param]) == 2:
-                tk.Label(self.leftFrame, text=self.sysParameters[param][0]).grid(
+            elif len(self.sysParameters[param]) == 2:"""
+            tk.Label(self.leftFrame, text=self.sysParameters[param][0]).grid(
                     row=counter, pady=2, sticky='W')
-                value = tk.StringVar()
-                entry = tk.Entry(self.leftFrame, textvariable=value)
-                entry.grid(row=counter, column=1, padx=10, sticky='W')
-                entry.insert(0, self.sysParameters[param][1])
-                entriesList.append(entry)
+            value = tk.StringVar()
+            entry = tk.Entry(self.leftFrame, textvariable=value, width=7)
+            entry.grid(row=counter, column=1, padx=10, sticky='W')
+            entry.insert(0, self.sysParameters[param][1])
+            entriesList.append(entry)
 
             self.entries[param] = entriesList
             counter += 1
@@ -185,6 +210,46 @@ class SimulationGui():
         self.btn2 = tk.Button(self.leftFrame, text="Clear plot",
                               command=self.clearplot_callback)
         self.btn2.grid(row=counter, column=1, sticky=tk.W+tk.E)
+    
+    def updateLeftFrame(self, var, val):
+        for key, value in self.sysParameters.items():
+                if var == value[0]:
+                    var = key
+                    break
+
+        row = self.entries[var][0]
+        if val == 1 and self.currentVars[1] != None:
+            try:
+                self.entries[self.currentVars[1]][2].destroy()
+                self.entries[self.currentVars[1]][3].destroy()
+                self.entries[self.currentVars[1]] = (self.entries[self.currentVars[1]][0],
+                                self.entries[self.currentVars[1]][1])
+            except:
+                pass
+
+        elif val == 2 and self.currentVars[2] != None:
+            try:
+                self.entries[self.currentVars[2]][2].destroy()
+                self.entries[self.currentVars[2]][3].destroy()
+                self.entries[self.currentVars[2]] = (self.entries[self.currentVars[2]][0],
+                                self.entries[self.currentVars[2]][1])
+            except:
+                pass
+
+        val1 = tk.StringVar()
+        val2 = tk.StringVar()
+        
+        entry1 = tk.Entry(self.leftFrame, textvariable=val1,width=7)
+        entry1.grid(row=row, column=2, padx=10, sticky='W')
+
+        entry2 = tk.Entry(self.leftFrame, textvariable=val2,width=7)
+        entry2.grid(row=row, column=3, padx=10, sticky='W')
+
+        self.entries[var] = (self.entries[var][0],
+                                self.entries[var][1], entry1, entry2)
+        
+        self.currentVars[val]=var
+
 
 
     def calculateButton_callback(self):
@@ -196,7 +261,7 @@ class SimulationGui():
         for key in self.entries:
             tempList = []
             tempList.append(self.tempSysParam[key][0])
-            for i in range(len(self.entries[key])):
+            for i in range(1,len(self.entries[key])):
                 try:
                     tempList.append(float(self.entries[key][i].get()))
                 except:
@@ -209,40 +274,53 @@ class SimulationGui():
         self.tempSysParam['deltaL'] = ('Total length (m)', (self.tempSysParam['H_Length'][1]) + (
             self.tempSysParam['V_Length'][1]) + self.tempSysParam['delta_L_bend'][1]*(self.tempSysParam['nbH'][1] + self.tempSysParam['nbV'][1]))
 
-        self.tempSysParam['Mfa'] = (self.tempSysParam['Mfa'][0], np.linspace(
-            self.tempSysParam['Mfa'][1], self.tempSysParam['Mfa'][2], int(self.tempSysParam['Mfa'][3])))
-        self.tempSysParam['Mfs'] = (self.tempSysParam['Mfs'][0], np.linspace(
-            self.tempSysParam['Mfs'][1], self.tempSysParam['Mfs'][2], int(self.tempSysParam['Mfs'][3])))
+        self.var1 = self.menu1.get()
+        self.var2 = self.menu2.get()
+
+        for key,value in self.tempSysParam.items():
+            if self.var1 == value[0]:
+                self.var1 = key
+            elif self.var2 == value[0]:
+                self.var2 = key
+
+        vary1 = np.linspace(self.tempSysParam[self.var1][1], self.tempSysParam[self.var1][2], int(self.tempSysParam[self.var1][3]))
+        vary2 = np.linspace(self.tempSysParam[self.var2][1], self.tempSysParam[self.var2][2], int(self.tempSysParam[self.var2][3]))
+
+        self.tempSysParam[self.var1] = (self.tempSysParam[self.var1][0],vary1)
+        self.tempSysParam[self.var2] = (self.tempSysParam[self.var2][0],vary2)
 
         self.pressureDrop = PressureCalc.pressureDropCalc(
-            self, **self.tempSysParam)
+            self, self.var1, self.var2, **self.tempSysParam)
         
         self.plotData(self.v.get())
 
     def plotData(self, val):
         # Executed when Pressure drop is clicked
         self.clearplot_callback()
-
+        
         if val == 1:  # Plot pressure drop per meter
             for i in range(len(self.pressureDrop)):
-                newList = [p/self.tempSysParam['deltaL'][1]
-                           for p in self.pressureDrop[i]]
-                self.ax.plot(self.tempSysParam['Mfa'][1], newList, lw=1)
+                newList = [p/self.tempSysParam['deltaL'][1] for p in self.pressureDrop[i]]
+                self.ax.plot(self.tempSysParam[self.var2][1], newList, lw=1)
 
             self.ax.set_ylabel("Pressure drop per meter (Pa/m)")
-            self.ax.set_xlabel("Mass flow air (kg/s)")
-            MfsLegend = [round(x, 2) for x in self.tempSysParam['Mfs'][1]]
+            
 
         elif val == 2:  # Plot power consumption
             for i in range(len(self.pressureDrop)):
-
-                power = [2*self.tempSysParam['Mfa'][1][j]*self.tempSysParam['R'][1]*self.tempSysParam['T'][1]*np.log(self.tempSysParam['pres'][1]/(
-                    self.tempSysParam['pres'][1]-self.pressureDrop[i][j]))/self.tempSysParam['deltaL'][1] for j in range(len(self.pressureDrop[i]))]
-                self.ax.plot(self.tempSysParam['Mfa'][1], power, lw=1)
+                power=[]
+                for j in range(len(self.pressureDrop[i])):
+                    if type(self.tempSysParam['Mfa'][1]) == list:
+                        power.append(2*self.tempSysParam['Mfa'][1][j]*self.tempSysParam['R'][1]*self.tempSysParam['T'][1]*np.log(self.tempSysParam['pres'][1]/(
+                            self.tempSysParam['pres'][1]-self.pressureDrop[i][j]))/self.tempSysParam['deltaL'][1])
+                    else:
+                        power.append(2*self.tempSysParam['Mfa'][1]*self.tempSysParam['R'][1]*self.tempSysParam['T'][1]*np.log(self.tempSysParam['pres'][1]/(
+                            self.tempSysParam['pres'][1]-self.pressureDrop[i][j]))/self.tempSysParam['deltaL'][1])
+                print(power)
+                print(self.tempSysParam[self.var2][1])
+                self.ax.plot(self.tempSysParam[self.var2][1], power, lw=1)
 
             self.ax.set_ylabel("Power consumption per meter (kW/m)")
-            self.ax.set_xlabel("Mass flow air (kg/s)")
-            MfsLegend = [round(x, 2) for x in self.tempSysParam['Mfs'][1]]
 
 
         elif val == 3:
@@ -253,23 +331,41 @@ class SimulationGui():
                 self.ax.plot(SLR, delta_P, lw=1)
 
             self.ax.set_ylabel("Pressure drop per meter (Pa/m)")
-            self.ax.set_xlabel("SLR")
-            MfsLegend = [round(x, 2) for x in self.tempSysParam['Mfs'][1]]
+
         
-        self.ax.legend(MfsLegend, bbox_to_anchor=(0.995, 0.9), title='Mass flow \nsolids (kg/s)')
+        self.ax.set_xlabel(self.tempSysParam[self.var2][0])
+
+        legendElements = [round(x, 2) if self.tempSysParam[self.var1][1][0]<100 else round(x, 0) for x in self.tempSysParam[self.var1][1]]
+        legendTitle = [i for i in self.tempSysParam[self.var1][0]]
+        
+        i = 11
+        while i < len(legendTitle):
+            legendTitle.insert(i,"\n")
+            i += 11    
+            
+        legendTitle = ''.join(legendTitle)
+
+        self.ax.legend(legendElements, bbox_to_anchor=(0.995, 0.9), title=legendTitle)
         self.canvas.draw()
 
+    
     def powerConsumptionButton_callback(self):
         pass
 
 
     def clearplot_callback(self):
-        # Clears the plot window
+        """
+        Clears the plot window
+        """
+
         self.ax.cla()
         self.canvas.draw()
 
     def quit_callback(self):
-        # Quits the GUI Application
+        """ 
+        Quits the GUI Application
+        """
+
         self.root.after_cancel(self.afterId)
         self.root.destroy()
 
--- Pressure.py
+++ Pressure.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env pythons
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import copy
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import csv
-import tkinter as tk
-
 
 class PressureCalc():
     def __init__(self):
@@ -32,7 +31,7 @@ class PressureCalc():
         self.pressureDrop = self.pressureDropCalc(self,sysParameters)    
         
 
-    def pressureDropCalc(self,**sysParam):
+    """ def pressureDropCalc(self,**sysParam):
         pressureDrop=[]
         mindeltaPx=[]
         mindeltaPy=[]
@@ -85,7 +84,8 @@ class PressureCalc():
 
             pressureDrop.append(tempList)
 
-        return pressureDrop
+        return pressureDrop"""
+        
 
 
     def plotOnlyPressure(self,calcData,**sysParam):
@@ -94,7 +94,6 @@ class PressureCalc():
 
         Mfa = np.linspace(sysParam['Mfa'][1], sysParam['Mfa'][2], sysParam['Mfa'][3])
         Mfs = np.linspace(sysParam['Mfs'][1], sysParam['Mfs'][2], sysParam['Mfs'][3])
-
 
         fig = Figure(figsize=(5,4), dpi=200)
         ax = fig.add_subplot(111)
@@ -109,6 +108,76 @@ class PressureCalc():
         fig.legend(Mfs, loc=5, title='Mass flow \nsolids (kg/s)')
         plt.rcParams.update({'font.size': 12})
         return fig
+
+
+    def pressureDropCalc(self,variable1,variable2, **sysParam):
+            """
+            Calculates the pressure drop of a pneumatic conveying system
+
+            Parameters
+            ----------
+            variable1 : str
+                Simulate pressure drop for this variable
+            variable2 : str
+                Simulate pressure drop for this variable
+
+            Returns
+            -------
+            Pandas dataframe
+            """
+            pressureDrop=[]
+            mindeltaPx=[]
+            mindeltaPy=[]
+            mindeltaSLR=[]
+
+            vary1 = sysParam[variable1][1]
+            vary2 = sysParam[variable2][1]
+
+            for i in vary1:
+                tempList = []
+                for j in vary2:
+                    sysParam[variable1] = (sysParam[variable1][0],i)
+                    sysParam[variable2] = (sysParam[variable2][0],j)
+
+                    Ac = np.pi*(sysParam['Diam'][1]**2) / 4  # Cross-sectional area (m^2)
+                    delta_L_bend = 2*np.pi * sysParam['Br'][1] * sysParam['Diam'][1]/4  # Length of 90deg bend(m)
+                    deltaL = (sysParam['H_Length'][1]) + (sysParam['V_Length'][1]) + delta_L_bend*(sysParam['nbH'][1] + sysParam['nbV'][1])  # Pipe length (m)
+                    deltaZ = sysParam['Br'][1] * sysParam['Diam'][1] * sysParam['nbV'][1] + sysParam['V_Length'][1]  # Pipe elevation change (m)
+
+                    sysParam['deltaL'] = ('Pipe length (m)',deltaL)
+
+                    rho_air = (sysParam['pres'][1]/(sysParam['R'][1]*1000*sysParam['T'][1])+1.89)/2  # kg/m3 Average density
+                    Vfa = sysParam['Mfa'][1]/(rho_air)  # Average volumetric flow rate air
+                    va = Vfa/Ac  # Average velocity air
+                    slr = sysParam['Mfs'][1]/sysParam['Mfa'][1]  # Solids loading ratio
+
+                    # solid and air friction factor - lambdaS/L
+                    Fri = va/(np.sqrt(sysParam['g'][1]*sysParam['Diam'][1]))  # Froude number
+                    lambdaS = 15.62 * slr**-0.6 * Fri**-1.93
+                    lambdaL = 0.316/(va*sysParam['Diam'][1]*rho_air/sysParam['dynVisc'][1])**0.25
+                    velRatio = 1-0.008*((1000*sysParam['dp'][1])**(0.3))*(sysParam['rho_p'][1]*0.65)**0.5
+
+                    # Pressure drops
+                    deltaPair = lambdaL*rho_air*va**2*deltaL/(2*sysParam['Diam'][1])
+                    deltaPacc = slr*va*rho_air*0.9*va
+                    deltaPsolids = slr*lambdaS*rho_air*va**2*deltaL/(2*sysParam['Diam'][1])
+                    deltaPbends = (1+slr)*(sysParam['nbH'][1]+sysParam['nbV'][1])*sysParam['ksiB'][1]*rho_air*va**2 / 2
+                    deltaPvertical = slr*rho_air*sysParam['g'][1]*deltaZ/velRatio
+
+                    deltaPtot = deltaPacc+deltaPair+deltaPsolids+deltaPbends+deltaPvertical
+
+                    tempList.append(deltaPtot)
+
+                min_value = min(tempList)
+                min_index = tempList.index(min_value)
+
+                """mindeltaPx.append(sysParam['Mfa'][1][min_index])
+                mindeltaSLR.append(sysParam['Mfs'][1][i]/sysParam['Mfa'][1][min_index])
+                mindeltaPy.append(min_value/deltaL)"""
+
+                pressureDrop.append(tempList)
+
+            return pressureDrop
 
 if __name__ == '__main__':
     sim_gui = PressureCalc()
